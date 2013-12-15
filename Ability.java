f8d8646b70fbb8b894148b1b32b4216d9c9b5dcc
onurrtan <onur.tan@windowslive.com>
2013-12-15 16:52:00 +0200

    Update Ability.java

--- Ability.java
+++ Ability.java
@@ -2,4 +2,34 @@
  * Created by turkmen on 15.12.2013.
  */
 public class Ability {
+  
+    private String coldness; // effective co efficent in urban areas
+    private double cost; // Current Cost they will update for each trade
+    private String drug;
+    private String temperature;
+    
+    
+    void changeAbility(){
+
+    }
+    protected void fillArray(){
+
+    }
+    
+    public String getColdness(){
+        return this.coldness;
+    }
+
+    public double getCost(){
+        return this.cost;
+    }
+
+    public String getDrug(){
+        return this.drug;
+    }
+    
+    public String getTemperature(){
+        return this.temperature;
+    }
+    
 }
