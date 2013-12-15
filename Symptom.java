ac4f94499875d0ad0a0f2a6b1e91bcc952f30479
onurrtan <onur.tan@windowslive.com>
2013-12-15 16:38:32 +0200

    Update Symptom.java

--- Symptom.java
+++ Symptom.java
@@ -2,6 +2,29 @@
  * Created by turkmen on 15.12.2013.
  */
 public class Symptom {
+  
+    private String cost; // effective co efficent in urban areas
+    private String infectivity; // Current Cost they will update for each trade
+    private String lethality;
+    
+    void changeSymptom(){
+
+    }
+    protected void fillArray(){
+
+    }
+    
+    public String getCost(){
+        return this.Cost;
+    }
+
+    public String getInfectivity(){
+        return this.infectivity;
+    }
+
+    public String getLethality(){
+        return this.lethality;
+    }
 
 
 }
